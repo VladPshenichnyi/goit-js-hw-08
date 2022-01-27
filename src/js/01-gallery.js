bd1130b69e29c97fed26d5c3077b40149d7cdd21
Vlad Pshenichnyi <PshenichnyiVlad@gmail.com>
2022-01-27 21:05:39 +0200

    started

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+galleryRef.innerHTML = createMarkup();
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: 'alt',
+    captionDelay: 300,
+});
+
+function createMarkup() {
+    let markup = "";
+    for (const {preview,original,description} of galleryItems) {
+        markup += `
+    <a class="gallery__item"  href="${original}"> 
+    <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+    </a>`;
+    }
+    return markup;
+}
