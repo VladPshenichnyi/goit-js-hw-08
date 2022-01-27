64eac6510a617d0253c4c740b33efdd93001b58d
Vlad Pshenichnyi <PshenichnyiVlad@gmail.com>
2022-01-28 01:18:05 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,17 +3,16 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe')
 const player = new Vimeo.Player(iframe);
-const LOCALSTORAGE_KEY = "videoplayer-current-time";
 
 player.on('timeupdate', throttle(function (time) {
     console.log(`Просмотр на : ${time.seconds} секунде`)
-    localStorage.setItem("LOCALSTORAGE_KEY", time.seconds);
+    localStorage.setItem("videoplayer-current-time", time.seconds);
     }, 1000),
 );
 
 let currentTime;
 try {
-    currentTime = localStorage.getItem('LOCALSTORAGE_KEY');
+    currentTime = localStorage.getItem("videoplayer-current-time");
     console.log(currentTime)
 } catch (error) {
     console.error("Get state error: ", error.message);
