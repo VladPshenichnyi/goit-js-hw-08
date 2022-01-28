e284c46196a1147498b693f9605f282520646727
Vlad Pshenichnyi <PshenichnyiVlad@gmail.com>
2022-01-29 01:36:51 +0200

    step by step

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,18 +3,33 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe')
 const player = new Vimeo.Player(iframe);
 
-player.on('timeupdate', throttle(function (time) {
-    console.log(`Просмотр на : ${time.seconds} секунде`)
-    localStorage.setItem("videoplayer-current-time", time.seconds);
+// ************************************************************************ 
+player.on('timeupdate', throttle(function (currentTime) {
+    console.log(`Просмотр на : ${JSON.stringify(currentTime)}`)
+    // console.log(currentTime)
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime)   );
     }, 1000),
 );
 
-let currentTime;
-try {
-    currentTime = localStorage.getItem("videoplayer-current-time");
-    console.log(currentTime)
-} catch (error) {
-    console.error("Get state error: ", error.message);
-}
+const saveCurrentTime = localStorage.getItem("videoplayer-current-time");
+// console.log(saveCurrentTime)
+const parsedCurrentTime = JSON.parse(saveCurrentTime)
+// console.log(parsedCurrentTime)
 
-player.setCurrentTime(currentTime)
+player.setCurrentTime(parsedCurrentTime.seconds)
+
+
+// *************************************************************************
+// player.on('timeupdate', throttle(function (time) {
+//     console.log(`Просмотр на : ${time.seconds} секунде`)
+//     localStorage.setItem("videoplayer-current-time", time.seconds);
+//     }, 1000),
+// );
+// let currentTime;
+// try {
+//     currentTime = localStorage.getItem("videoplayer-current-time");
+//     console.log(currentTime)
+// } catch (error) {
+//     console.error("Get state error: ", error.message);
+// }
+// player.setCurrentTime(currentTime)
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    text: document.querySelector('.feedback-form textarea')
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormData, 500))
+
+function onFormSubmit(evt) { 
+    evt.preventDefault();
+    console.log('Отправляем форму и очищаем локальное хранилище');
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY)
+}
+
+function onFormData(e) { 
+    formData[e.target.name] = e.target.value;    
+    console.log(formData)
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));  
+    console.log()
+}
+
+saveFormData()
+function saveFormData() { 
+    try {
+        const savedFormData = localStorage.getItem(STORAGE_KEY);
+        console.log(savedFormData)
+        const parsedFormData = JSON.parse(savedFormData); 
+        console.log(parsedFormData)
+        if (parsedFormData) { 
+            refs.email.value = parsedFormData.email || "";
+            refs.text.value = parsedFormData.message || "";
+        }        
+    } catch (error) {
+    console.error("Get state error: ", error.message);
+    }
+}
 
