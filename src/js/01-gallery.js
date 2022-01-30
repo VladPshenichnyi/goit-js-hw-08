9a3376f87929cb8d8083c141e9cf244626fbffec
Vlad Pshenichnyi <PshenichnyiVlad@gmail.com>
2022-01-30 17:59:23 +0200

    ref
    Переделана таска под разметку через hbs

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,22 +1,34 @@
-import { galleryItems } from './gallery-items';
+import imagesTpl from '../templates/images.hbs';
+import galleryItems from './galleryItems.json';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+
 const galleryRef = document.querySelector('.gallery');
-galleryRef.innerHTML = createMarkup();
+const galletyMarkup = createImagesMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', galletyMarkup);
+
+function createImagesMarkup(galleryItems) { 
+    return imagesTpl(galleryItems);
+}
 
 const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 300,
 });
 
-function createMarkup() {
-    let markup = "";
-    for (const {preview,original,description} of galleryItems) {
-        markup += `
-    <a class="gallery__item"  href="${original}"> 
-    <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
-    </a>`;
-    }
-    return markup;
-}
+// const markup = itemsTemplate(imagesTpl)
+// galleryRef.innerHTML = createMarkup();
+
+
+// function createMarkup() {
+//     let markup = "";
+//     for (const {preview,original,description} of galleryItems) {
+//         markup += `
+//     <a class="gallery__item"  href="${original}"> 
+//     <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+//     </a>`;
+//     }
+//     return markup;
+// }
